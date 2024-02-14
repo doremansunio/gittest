fda4c0854362b992cdc47a9fd1df6abd1927851b
Phani <Phani@gmail.com>
2024-02-14 12:30:51 +0530

    g1

--- main.tf
+++ main.tf
@@ -1,48 +1,62 @@
-terraform {
-  required_providers {
-    rafay = {
-      source = "RafaySystems/rafay"
-      version = "1.1.22"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "github" {
-  #token = "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"  
-}
-
-# resource "local_file" "netpolicy-file" {
-#   //depends_on = [ rafay_cluster_sharing.demo-terraform-specific ]
-#   //depends_on = [rafay_groupassociation.group-association]
-#   filename = "${var.project_name}-within-ws-rule.yaml"
-#   content = templatefile("${path.module}/net-policy-template.yaml", {
-#     project_name = var.project_name
-#   })
+# terraform {
+#   required_providers {
+#     rafay = {
+#       source = "RafaySystems/rafay"
+#       version = "1.1.22"
+#     }
+#     github = {
+#       source  = "integrations/github"
+#       version = "~> 4.0"
+#     }
+#   }
 # }
 
-data "template_file" "example" {
-    template = file("${path.module}/net-policy-template.yaml")
-    vars = {
-      project_name = var.project_name
-    }
+# provider "github" {
+#   #token = "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"  
+# }
+
+# # resource "local_file" "netpolicy-file" {
+# #   //depends_on = [ rafay_cluster_sharing.demo-terraform-specific ]
+# #   //depends_on = [rafay_groupassociation.group-association]
+# #   filename = "${var.project_name}-within-ws-rule.yaml"
+# #   content = templatefile("${path.module}/net-policy-template.yaml", {
+# #     project_name = var.project_name
+# #   })
+# # }
+
+# data "template_file" "example" {
+#     template = file("${path.module}/net-policy-template.yaml")
+#     vars = {
+#       project_name = var.project_name
+#     }
+# }
+
+
+
+
+
+# resource "github_repository_file" "netgitfile" {
+# #   repository          = "/"
+# #   branch              = "main"
+# #   file                = "${var.project_name}-within-ws-rule.yaml"
+# #   content             = local_file.netpolicy-file.content
+# #   commit_message      = "Managed by Terraform"
+# #   overwrite_on_create = true
+#     repository = "gittest"
+#     branch= "main"
+#     file="loca_file.yaml"
+#     content = data.template_file.example.rendered
+# }
+
+
+provider "github" {
+  token   = "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"
+  owner   = "doremansunio"
 }
 
-
-resource "github_repository_file" "netgitfile" {
-#   repository          = "/"
-#   branch              = "main"
-#   file                = "${var.project_name}-within-ws-rule.yaml"
-#   content             = local_file.netpolicy-file.content
-#   commit_message      = "Managed by Terraform"
-#   overwrite_on_create = true
-    repository = "gittest"
-    branch= "main"
-    file="loca_file.yaml"
-    content = data.template_file.example.rendered
+resource "github_repository_file" "readme" {
+  repository     = "gittest"
+  file           = "README.md"
+  content        = "# Awesome Project\nThis is an Awesome Project!"
+  overwrite_on_create = true
 }
-
-
