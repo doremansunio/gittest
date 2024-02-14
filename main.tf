d09ab6c74521b82c9321f9c1ccc9fc87ce82c7a9
Phani <Phani@gmail.com>
2024-02-14 11:36:12 +0530

    v3

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 provider "github" {
-  #token = "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"  
+  token = "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"  
 }
 
 resource "local_file" "netpolicy-file" {
@@ -23,6 +23,8 @@ resource "local_file" "netpolicy-file" {
     project_name = var.project_name
   })
 }
+
+
 
 resource "null_resource" "name1" {
     provisioner "local-exec" {
