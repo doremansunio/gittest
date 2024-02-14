29d82e08ab8b8200c87cdbe44a5e2649dff855ea
Phani <Phani@gmail.com>
2024-02-14 11:27:08 +0530

    v1

--- main.tf
+++ main.tf
@@ -42,9 +42,10 @@ resource "github_repository_file" "netgitfile" {
 #   content             = local_file.netpolicy-file.content
 #   commit_message      = "Managed by Terraform"
 #   overwrite_on_create = true
-    repository = "doemansunio/gittest"
-    file="loca_file.txt"
-    content = "local_file.txt"
+    repository = "gittest"
+    branch= "main"
+    file="loca_file.yaml"
+    content = local_file.netpolicy-file.content
 }
 
 
